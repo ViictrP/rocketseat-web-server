ab70ecad20ba1e608700c19f04955fb3b42f51f5
Victor <victor.prado@capco.com>
2022-06-13 16:18:34 -0300

    cors added

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,15 @@
-import express from 'express';
+import express from 'express'
+import cors from 'cors'
 
-import { createFeedbackUseCaseAdapter } from './rest';
+import {createFeedbackUseCaseAdapter} from './rest';
 
 const app = express()
+
+app.use(cors())
 app.use(express.json())
 
 app.post('/feedbacks', createFeedbackUseCaseAdapter)
 
 app.listen(3333, () => {
-  console.log('HTTP server running at port 3333')
+	console.log('HTTP server running at port 3333')
 })
